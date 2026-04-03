c1add9bfea4db1f20d57abc73746ffdd0d14bb1a
hijab10 <hijab.zahid@yahoo.com>
2026-04-03 15:31:34 +0200

    compose image changes

--- batch-pipelines/airflow/dags/ingest_orders.py
+++ batch-pipelines/airflow/dags/ingest_orders.py
@@ -1,33 +1,33 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
+import logging
 import sys
-import os
 
-# Add scripts folder to path
-sys.path.append("/opt/airflow/scripts") 
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
 
+sys.path.append("/opt/airflow/scripts")
 from load_transactions import main as load_data
 
 
-default_args = {
-    "owner": "hijab",
-    "start_date": datetime(2024, 1, 1),
-    "retries": 1,
-}
+def run_ingestion():
+    logging.info("Starting ingestion")
+    load_data()
+    logging.info("Finished ingestion")
+
 
 with DAG(
     dag_id="ingest_orders",
-    default_args=default_args,
-    schedule=None,  # manual trigger
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
     catchup=False,
-    description="Load online retail dataset into Postgres",
+    default_args={
+        "owner": "hijab",
+        "retries": 1,
+    },
+    description="Load online retail CSV into Postgres",
 ) as dag:
-
-    ingest_task = PythonOperator(
+    PythonOperator(
         task_id="load_raw_transactions",
-        python_callable=load_data,
+        python_callable=run_ingestion,
         execution_timeout=timedelta(minutes=10),
     )
-
-    ingest_task
